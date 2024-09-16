19b69bb6b9445ecd02572ae9b12906c49136d013
bapic <itsbapic@gmail.com>
2024-09-16 15:25:34 -0700

    Remove Posthog logs

--- src/plugins/posthog.js
+++ src/plugins/posthog.js
@@ -3,8 +3,8 @@ import posthog from 'posthog-js'
 const POSTHOG_API_KEY = import.meta.env.VITE_POSTHOG_API_KEY
 const POSTHOG_HOST = import.meta.env.VITE_POSTHOG_HOST
 
-console.log('PostHog API Key:', POSTHOG_API_KEY.substring(0, 8) + '...')
-console.log('PostHog Host:', POSTHOG_HOST.substring(0, 8) + '...')
+// console.log('PostHog API Key:', POSTHOG_API_KEY.substring(0, 8) + '...')
+// console.log('PostHog Host:', POSTHOG_HOST.substring(0, 8) + '...')
 
 export default {
   install(app) {
@@ -20,6 +20,6 @@ export default {
 
     app.config.globalProperties.$posthog = posthogInstance
 
-    console.log('PostHog initialized:', posthogInstance)
+    // console.log('PostHog initialized:', posthogInstance)
   }
 }
