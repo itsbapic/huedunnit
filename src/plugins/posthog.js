64e28fe37e0266108734f1de37aae39de8274979
bapic <itsbapic@gmail.com>
2024-09-16 15:23:56 -0700

    Posthog fixes

--- src/plugins/posthog.js
+++ src/plugins/posthog.js
@@ -3,10 +3,23 @@ import posthog from 'posthog-js'
 const POSTHOG_API_KEY = import.meta.env.VITE_POSTHOG_API_KEY
 const POSTHOG_HOST = import.meta.env.VITE_POSTHOG_HOST
 
+console.log('PostHog API Key:', POSTHOG_API_KEY.substring(0, 8) + '...')
+console.log('PostHog Host:', POSTHOG_HOST.substring(0, 8) + '...')
+
 export default {
   install(app) {
-    app.config.globalProperties.$posthog = posthog.init(POSTHOG_API_KEY, {
-      api_host: POSTHOG_HOST
+    if (!POSTHOG_API_KEY) {
+      console.error('PostHog API key is missing')
+      return
+    }
+
+    const posthogInstance = posthog.init(POSTHOG_API_KEY, {
+      api_host: POSTHOG_HOST,
+      debug: true // Enable debug mode
     })
+
+    app.config.globalProperties.$posthog = posthogInstance
+
+    console.log('PostHog initialized:', posthogInstance)
   }
 }
