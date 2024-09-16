23a2c7f99d3f2becba5a25a9aaf6c7f5fc106658
bapic <itsbapic@gmail.com>
2024-09-16 14:46:21 -0700

    Analytics Hotfix

--- src/plugins/posthog.js
+++ src/plugins/posthog.js
@@ -1,9 +1,12 @@
 import posthog from 'posthog-js'
 
+const POSTHOG_API_KEY = import.meta.env.VITE_POSTHOG_API_KEY
+const POSTHOG_HOST = import.meta.env.VITE_POSTHOG_HOST
+
 export default {
   install(app) {
-    app.config.globalProperties.$posthog = posthog.init(process.env.VITE_POSTHOG_API_KEY, {
-      api_host: process.env.VITE_POSTHOG_HOST
+    app.config.globalProperties.$posthog = posthog.init(POSTHOG_API_KEY, {
+      api_host: POSTHOG_HOST
     })
   }
 }
